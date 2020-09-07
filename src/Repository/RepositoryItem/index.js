89453d49d18b80dbb5c6e1c7c947dde606d39c03
mbrad26 <mbrad26@gmail.com>
2020-09-07 17:45:42 +0100

    extract Button component and add mutation

--- src/Repository/RepositoryItem/index.js
+++ src/Repository/RepositoryItem/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useMutation, gql } from '@apollo/client';
 
 import '../style.css';
 import Link from '../../Link';
+import Button from '../../Button';
+
+const STAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    addStar(input: { starrableId: $id}) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
 
 const RepositoryItem = ({
+  id,
   url,
   name,
   owner,
@@ -13,39 +27,53 @@ const RepositoryItem = ({
   primaryLanguage,
   viewerHasStarred,
   viewerSubscription,
-}) => (
-  <div>
-    <div className="RepositoryItem-title">
-      <h2>
-        <Link href={url}>{name}</Link>
-      </h2>
+}) => {
+  const [addStar, { data }] = useMutation(STAR_REPOSITORY);
+  console.log('MUTATION: ', data);
 
-      <div className="RepositoryItem-title-action">
-        {stargazers.totalCount} Stars
-      </div>
-    </div>
+  return (
+    <div>
+      <div className="RepositoryItem-title">
+        <h2>
+          <Link href={url}>{name}</Link>
+        </h2>
 
-    <div className="RepositoryItem-description">
-      <div
-        className="RepositoryItem-description-info"
-        dangerouslySetInnerHTML={{ __html: descriptionHTML }}
-      />
-      <div className="RepositoryItem-description-details">
-        <div>
-          {primaryLanguage && (
-            <span>Language: {primaryLanguage.name}</span>
-          )}
+        <div className="RepositoryItem-title-action">
+          {stargazers.totalCount} Stars
         </div>
+
         <div>
-          {owner && (
-            <span>
-              Owner: <a href={owner.url}>{owner.login}</a>
-            </span>
-          )}
+          <Button
+            className={'RepositoryItem-title-action'}
+            onClick={() => addStar({ variables: { id }})}
+          >
+            {stargazers.totalCount} Star
+          </Button>
         </div>
       </div>
+
+      <div className="RepositoryItem-description">
+        <div
+          className="RepositoryItem-description-info"
+          dangerouslySetInnerHTML={{ __html: descriptionHTML }}
+        />
+        <div className="RepositoryItem-description-details">
+          <div>
+            {primaryLanguage && (
+              <span>Language: {primaryLanguage.name}</span>
+            )}
+          </div>
+          <div>
+            {owner && (
+              <span>
+                Owner: <a href={owner.url}>{owner.login}</a>
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default RepositoryItem;
