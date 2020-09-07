c9a836b0a61a0fd7c5e1c773359712e6b9716bc3
mbrad26 <mbrad26@gmail.com>
2020-09-07 14:03:29 +0100

    update snapshot test

--- src/App/index.test.js
+++ src/App/index.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { MockedProvider } from "@apollo/client/testing";
 import { render, screen } from '@testing-library/react';
 
 import App from './';
 
 describe('App', () => {
   it('renders snapshot', () => {
-    const { container } = render(<App />);
+    const { container } = render(
+      <MockedProvider client={[]}>
+        <App />
+      </MockedProvider>
+    );
 
     expect(container.firstChild).toMatchSnapshot();
   })
