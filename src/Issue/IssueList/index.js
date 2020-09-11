03c9c40fead56256da7c955a34a4f978a00614fc
mbrad26 <mbrad26@gmail.com>
2020-09-11 13:12:33 +0100

    add client side filtering

--- src/Issue/IssueList/index.js
+++ src/Issue/IssueList/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
 
 import './style.css';
 import Loading from '../../Loading'; 
 import IssueItem from '../IssueItem'; 
 import ErrorMessage from '../../Error';
+import { ButtonUnobtrusive } from '../../Button';
 
 
 const GET_ISSUES_OF_REPOSITORY = gql`
@@ -26,14 +27,39 @@ const GET_ISSUES_OF_REPOSITORY = gql`
   }
 `;
 
+const ISSUE_STATES = {
+  NONE: 'NONE',
+  OPEN: 'OPEN',
+  CLOSED: 'CLOSED',
+};
+
+const TRANSITION_LABELS = { 
+  [ISSUE_STATES.NONE]: 'Show Open Issues', 
+  [ISSUE_STATES.OPEN]: 'Show Closed Issues', 
+  [ISSUE_STATES.CLOSED]: 'Hide Issues',
+};
+
+const TRANSITION_STATE = { 
+  [ISSUE_STATES.NONE]: ISSUE_STATES.OPEN, 
+  [ISSUE_STATES.OPEN]: ISSUE_STATES.CLOSED,
+  [ISSUE_STATES.CLOSED]: ISSUE_STATES.NONE,
+}
+
+const isShow = issueState => issueState != ISSUE_STATES.NONE;
+
 const Issues = ({ 
   repositoryName, 
   repositoryOwner 
 }) => {
+  const [issueState, setIssueState] = useState(ISSUE_STATES.NONE);
   const { loading, error, data } = useQuery(
     GET_ISSUES_OF_REPOSITORY,
     { variables: { repositoryOwner, repositoryName }},
     );
+  
+  const onChangeIssueState = nextIssueState => {
+    setIssueState(nextIssueState);
+  }
 
   if(loading) return <Loading />;
   if(error) return <ErrorMessage error={error} />;
@@ -41,9 +67,26 @@ const Issues = ({
     return <div className='IssueList'>No issues ...</div>
   };
 
+  const filtereredRepository = {
+    issues: {
+      edges: data.repository.issues.edges.filter(
+        issue => issue.node.state === issueState,
+      ),
+    },
+  };
+
+  if(!filtereredRepository.issues.edges.length) {
+    return <div className="IssueList">No issues ...</div>;
+  }
+
   return (
     <div className='Issues'>
-      <IssueList issues={data.repository.issues} />
+      <ButtonUnobtrusive
+        onClick={() => onChangeIssueState(TRANSITION_STATE[issueState])}
+      >
+        {TRANSITION_LABELS[issueState]}
+      </ButtonUnobtrusive>
+      {isShow(issueState) && <IssueList issues={filtereredRepository.issues} />}
     </div>
   );
 };
